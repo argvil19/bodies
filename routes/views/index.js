9bed3569b082213f6dc4ec83be3b98f0bbb398d8
Shturman <mrshturman@gmail.com>
2017-01-08 06:14:23 +0700

    Added recent section for homepage route (/routes/views/index.js)

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,6 +1,7 @@
 var keystone = require('keystone');
+var PostCategory = keystone.list('PostCategory').model;
 
-exports = module.exports = function (req, res) {
+exports = module.exports = function (req, res, next) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
@@ -9,6 +10,19 @@ exports = module.exports = function (req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 
-	// Render the view
-	view.render('index');
+	PostCategory.find({
+			published: true
+	})
+		.sort({creationDate: -1})
+		.limit(8)
+		.exec(function(err, categories){
+			if (err || !categories.length) {
+				return next(err);
+			}
+			
+			locals.recent = categories;
+
+			// Render the view
+			view.render('index', locals);
+		});
 };
