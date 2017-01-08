b2a90f1eb12e58c43c61e8a770278b4c7198c1f1
NelbiAlvarado <joseegnzalez@gmail.com>
2017-01-07 20:29:54 -0400

    Changes on the home page - Showing the most recent series - When clicking on a series, redirect to details.

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,14 +1,23 @@
 var keystone = require('keystone');
-
-exports = module.exports = function (req, res) {
-
+var PostCategory = keystone.list('PostCategory').model;
+exports = module.exports = function (req, res, next) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
-	locals.section = 'Home';
-
-	// Render the view
-	view.render('index');
+	locals.section = 'home';
+	PostCategory.find({
+			published: true
+	})
+		.sort({creationDate: -1})
+		.limit(8)
+		.exec(function(err, categories){
+			if (err || !categories.length) {
+				return next(err);
+			}
+			
+			locals.recent = categories;
+			// Render the view
+			view.render('index', locals);
+		});
 };
